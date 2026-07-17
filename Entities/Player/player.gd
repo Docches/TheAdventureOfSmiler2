66ee756fd801c02f9580047c9fe855c3ad726ee6
Docches <leoblade08@gmail.com>
2026-07-17 20:06:08 +0200

    Added Village map

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 @onready var animation_player = $Sprite/PlayerAnimation
 
 var movement_enabled = true
-var speed = 10
+var speed = 100.0
 
 func _ready():
 	add_to_group("player")
@@ -13,28 +13,36 @@ func _ready():
 	position.y = GlobalVar.player_position_y
 
 func _physics_process(delta: float) -> void:
-	# movement
 	if movement_enabled:
 		var direzioneO = Input.get_axis("move_left", "move_right")
 		var direzioneV = Input.get_axis("move_up", "move_down")
 		
-		position.x += direzioneO * speed
-		position.y += direzioneV * speed
+		var direction = Vector2(direzioneO, direzioneV)
+		
+		if direction.length() > 0:
+			direction = direction.normalized()
+			
+		velocity = direction * speed
+		
+		move_and_slide()
 		
 		GlobalVar.player_position_x = position.x
 		GlobalVar.player_position_y = position.y
 		
-		if(direzioneO<0):
+		if direzioneO < 0:
 			animation_player.play("walk_left")
-		elif(direzioneO>0):
+		elif direzioneO > 0:
 			animation_player.play("walk_right")
-		elif(direzioneV>0):
+		elif direzioneV > 0:
 			animation_player.play("walk_down")
-		elif(direzioneV<0):
+		elif direzioneV < 0:
 			animation_player.play("walk_up")
 		else:
 			animation_player.stop()
 			Sprite.frame_coords.y = 0
+	else:
+		velocity = Vector2.ZERO
+		move_and_slide()
 
 func wait_dialogue_end():
 	animation_player.stop()
